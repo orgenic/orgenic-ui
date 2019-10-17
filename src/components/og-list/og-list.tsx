6b3015d1f63f7eb9167ee4f89695d22ab4840b43
Timo Krieger <timo.krieger@maximago.de>
2019-10-17 12:17:23 +0200

    refactored multiselection; added required attr

--- src/components/og-list/og-list.tsx
+++ src/components/og-list/og-list.tsx
@@ -4,7 +4,7 @@
  * See LICENSE file at https://github.com/orgenic/orgenic-ui/blob/master/LICENSE
  **/
 
-import { h, Component, Prop, EventEmitter, Event } from '@stencil/core';
+import { h, Component, Prop, EventEmitter, Event, Watch, State } from '@stencil/core';
 
 @Component({
   tag: 'og-list',
@@ -12,12 +12,25 @@ import { h, Component, Prop, EventEmitter, Event } from '@stencil/core';
   shadow: true
 })
 export class OgList {
-  
+
   /**
    * The key of the selected list item
-   */  
-  @Prop({ mutable: true })
-  public selected;
+   */
+  @Prop({ mutable: true, reflect: true })
+  public selected: string | string[];
+
+  @Watch('selected')
+  public handleSelectedPropChanged(newValue: string | string[]) {
+    if (this.multiselect) {
+      // try to parse html encoded string to array
+      if (typeof newValue === 'string') {
+        newValue = JSON.parse(newValue);
+      }
+      this.internalSelection = new Set(newValue as string[]);
+    } else {
+      this.internalSelection = new Set([ newValue as string ]);
+    }
+  }
 
   /**
    * An array of items to choose from
@@ -68,6 +81,12 @@ export class OgList {
   public multiselect: boolean;
 
   /**
+   * Requires a selection of at least one item. If one item is selected it prevents the user from deselecting it
+   */
+  @Prop()
+  public required: boolean;
+
+  /**
    * Determines, whether the control is disabled or not
    */
   @Prop()
@@ -79,18 +98,40 @@ export class OgList {
   @Event()
   public itemSelected: EventEmitter<any>;
 
+  @State()
+  private internalSelection: Set<string> = new Set();
+
+  public componentDidLoad() {
+    this.handleSelectedPropChanged(this.selected);
+  }
+
   public listItemSelected(item: any): void {
     if (!this.disabled && !this.isItemDisabled(item)) {
+      const value = this.getKeyValue(item);
       if (this.isItemSelected(item)) {
-        this.selected.delete(item[this.keyProperty] + '');
-      } else {
-        if (!this.multiselect) {
-          this.selected.clear();
+        // shall we deny toggle selection if we are not using multi selection?
+        if (this.required && this.internalSelection.size === 1) {
+          return;
         }
-        this.selected.add(item[this.keyProperty] + '');
-        this.itemSelected.emit(this.selected.values());
+        if (this.multiselect) {
+          this.internalSelection.delete(value);
+          this.internalSelection = new Set(this.internalSelection);
+          this.selected = [ ...this.internalSelection ];
+        } else {
+          this.internalSelection = new Set();
+          this.selected = '';
+        }
+      } else {
+        if (this.multiselect) {
+          this.selected = [ ...this.internalSelection, value ];
+          this.internalSelection = new Set(this.selected);
+        } else {
+          this.internalSelection = new Set([ value ]);
+          this.selected = value;
+        }
       }
-      this.selected = new Set(this.selected.values());
+      this.itemSelected.emit(this.selected);
+      console.log('this.selected', this.selected);
     }
   }
 
@@ -99,32 +140,25 @@ export class OgList {
   }
 
   public isItemSelected(item: any): boolean {
-    return this.selected.has(item[this.keyProperty]);
+    return this.internalSelection.has(this.getKeyValue(item));
   }
 
   private isItemDisabled(item: any): boolean {
     return item[this.disabledProperty] || false;
   }
 
+  private getKeyValue(item: any): string {
+    return item[this.keyProperty] + '';
+  }
+
   public render(): HTMLElement {
-    if (typeof this.selected === "string") {
-      let selection = this.selected.trim().split(" ");
-      
-      this.selected = new Set();      
-      selection.forEach(item => this.selected.add(item));
-    }
-
-    if (typeof this.selected === "undefined") {
-      this.selected = new Set();
-    }
-
     return <ul class="og-list">
       {
         !this.hasValidItems()
           ? <li>{this.emptyListMessage}</li>
           : this.items.map((item): HTMLElement =>
             <og-list-item
-              key={item[this.keyProperty]}
+              key={this.getKeyValue(item)}
               label={item[this.labelProperty]}
               show-image={!!this.imageUrlProperty}
               image={item[this.imageUrlProperty]}
